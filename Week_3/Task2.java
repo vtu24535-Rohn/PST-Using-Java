d837b053a6bcadc9b783541a8a351d1ee13fec92
vtu24535-Rohn <vtu24535@veltech.edu.in>
2026-01-23 11:26:31 +0530

    Add files via upload

---
+++ Week_3/Task2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Task2{
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+        for (int i = 1; i <= s.length() - k; i++) {
+        String substr = s.substring(i, i + k);
+
+        if (substr.compareTo(smallest) < 0) {
+            smallest = substr;
+        }
+        if (substr.compareTo(largest) > 0) {
+            largest = substr;
+        }
+    }
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
